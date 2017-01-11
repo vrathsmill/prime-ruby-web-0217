0e372185eff5aac60c3c6df9861f425d55557943
Victoria Rathsmill <vlrathsmill@gmail.com>
2017-01-11 21:48:47 +0000

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,14 @@
 # Add  code here!
+
+def prime?(number)
+  if number < 2
+    return false
+  end
+prime_count = 2...number
+  for prime in prime_count
+    if number % prime == 0
+      return false
+    end
+  end
+  return true 
+end
